d16db61230ed9fb0285f87ebdbdcd56fdf3bafea
Nastya <femalenastya@gmail.com>
2026-06-10 18:12:50 +0200

    [Sprint_4] Cosmetic changes: added descriptions to README.md and test_bookcollector.py files

--- conftest.py
+++ conftest.py
@@ -6,15 +6,18 @@ def books_with_genres():
         ['fiction_book1','Фантастика'],
         ['fiction_book2','Фантастика'],
         ['fiction_book3','Фантастика'],
+        ['fiction_book4', 'Фантастика'],
         ['horror_book1','Ужасы'],
         ['horror_book2','Ужасы'],
         ['detective_book1','Детективы'],
         ['detective_book2','Детективы'],
         ['detective_book3','Детективы'],
         ['detective_book4','Детективы'],
+        ['detective_book5', 'Детективы'],
         ['cartoon_book1','Мультфильмы'],
         ['comedy_book1','Комедии'],
-        ['comedy_book2','Комедии']
+        ['comedy_book2','Комедии'],
+        ['comedy_book3', 'Комедии']
     ]
 
 @pytest.fixture(scope='session')
--- test_bookcollector.py
+++ test_bookcollector.py
@@ -23,14 +23,14 @@ class TestBooksCollector:
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
-    # 1 test: BooksCollector.add_new_book() method for book longer than 40 characters in title
+    # test 1: BooksCollector.add_new_book() method for book longer than 40 characters in title
     # ER: it shouldn't be added
     def test_add_new_book_booktitle_more_than40_char_not_added(self):
         collector = BooksCollector()
         collector.add_new_book('Странная история доктора Джекила и мистера Хайда')
         assert len(collector.books_genre) == 0
 
-    # 2 test: BookCollector.add_new_book() method for second entry of the same book
+    # test 2: BookCollector.add_new_book() method for second entry of the same book
     # ER: it shouldn't be added
     def test_add_new_book_already_added_to_dict_secondary_not_added(self):
         collector = BooksCollector()
@@ -46,8 +46,8 @@ class TestBooksCollector:
         ['Комедии', 'Комедии']
     ]
 
-    # 3 test:
-    # ER:
+    # test 3: set a book per valid genre
+    # ER: every valid genre is set for the book
     @pytest.mark.parametrize('title, genre', books)
     def test_set_book_genre_five_genres_all_set(self, title, genre):
         collector = BooksCollector()
@@ -55,8 +55,8 @@ class TestBooksCollector:
         collector.set_book_genre(title, genre)
         assert collector.books_genre[title] == genre
 
-    # 4 test:
-    # ER:
+    # test 4: set invalid (not present in BookCollector.genre) for the book
+    # ER: genre is NOT set and is left empty
     def test_set_book_genre_invalid_genre_not_set(self):
         collector = BooksCollector()
         title = 'invalid_book'
@@ -65,15 +65,15 @@ class TestBooksCollector:
         collector.set_book_genre(title, genre)
         assert collector.books_genre[title] == ''
 
-    # 5 test:
-    # ER:
+    # test 5: set genre for non-existent book
+    # ER: nothing happens
     def test_set_book_genre_book_not_added_to_dict_not_set(self):
         collector = BooksCollector()
         collector.set_book_genre('Клуб убийств по четвергам', 'Детективы')
         assert len(collector.books_genre) == 0
 
-    # 6 test:
-    # ER:
+    # test 6: get valid genre for existent book by title
+    # ER: genre is correctly returned as specified in BookCollector.books_genre
     @pytest.mark.parametrize('title, genre', books)
     def test_get_book_genre_all_genres_returned(self, title, genre):
         collector = BooksCollector()
@@ -81,8 +81,8 @@ class TestBooksCollector:
         collector.set_book_genre(title, genre)
         assert collector.get_book_genre(title) == genre
 
-    # 7 test:
-    # ER:
+    # test 7: filter existent books by valid genre
+    # ER: only books with specified genres are returned from the dict of books with different genres
     def test_get_books_with_specific_genre_all_genres_returned(self, books_with_genres, genres):
         collector = BooksCollector()
         for book in books_with_genres:
@@ -93,22 +93,22 @@ class TestBooksCollector:
             collection_length_expected = len(list(filter(lambda x:genre in x, books_with_genres)))
             assert collection_length_actual == collection_length_expected
 
-    # 8 test:
-    # ER:
+    # test 8: get dict of books with books present
+    # ER: dict is returned
     def test_get_books_genre_all_books_returned(self):
         collector = BooksCollector()
         collector.add_new_book('book1')
         collector.add_new_book('book2')
         collector.add_new_book('book3')
-        assert len(collector.get_books_genre()) == len(collector.books_genre)
+        assert collector.get_books_genre() == collector.books_genre
 
     kids_books = [
         ['kids_book1','Фантастика'],
         ['kids_book2','Мультфильмы'],
         ['kids_book3','Комедии'],
     ]
-    # 9 test:
-    # ER:
+    # test 9: get books with kid-safe genres
+    # ER: kid-safe books are returned
     @pytest.mark.parametrize('title, genre', kids_books)
     def test_get_books_for_children_kids_only_return_all(self, title, genre):
         collector = BooksCollector()
@@ -121,8 +121,8 @@ class TestBooksCollector:
         ['not_kids_book2', 'Детективы']
     ]
 
-    # 10 test:
-    # ER:
+    # test 10: filter books that are not kid-safe
+    # ER: nothing is returned
     @pytest.mark.parametrize('title, genre', not_kids_books)
     def test_get_books_for_children_not_kids_books_not_return(self, title, genre):
         collector = BooksCollector()
@@ -130,16 +130,16 @@ class TestBooksCollector:
         collector.set_book_genre(title, genre)
         assert title not in collector.get_books_for_children()
 
-    # test 11:
-    # ER:
+    # test 11: existent books is added to favorites
+    # ER: book is in favorites
     def test_add_book_in_favorites_new_book_added(self):
         collector = BooksCollector()
         collector.add_new_book('favorite_book1')
         collector.add_book_in_favorites('favorite_book1')
         assert collector.favorites == ['favorite_book1']
 
-    # test 12:
-    # ER:
+    # test 12: double entries of the same book in favorites
+    # ER: book entries are not doubled in favorites
     def test_add_book_in_favorites_present_book_not_added(self):
         collector = BooksCollector()
         collector.add_new_book('favorite_book1')
@@ -147,15 +147,15 @@ class TestBooksCollector:
         collector.add_book_in_favorites('favorite_book1')
         assert len(collector.favorites) == 1
 
-    # test 13:
-    # ER:
+    # test 13: add non-existent book to favorites
+    # ER: nothing is added
     def test_add_book_in_favorites_missing_book_not_added(self):
         collector = BooksCollector()
         collector.add_book_in_favorites('favorite_book1')
         assert collector.favorites == []
 
-    # test 14:
-    # ER:
+    # test 14: present in favorites book is deleted
+    # ER: book is deleted
     def test_delete_book_from_favorites_present_book_deleted(self):
         collector = BooksCollector()
         collector.add_new_book('favorite_book1')
@@ -163,8 +163,8 @@ class TestBooksCollector:
         collector.delete_book_from_favorites('favorite_book1')
         assert collector.favorites == []
 
-    # test 15:
-    # ER:
+    # test 15: get list of favorites
+    # ER: correct list is returned
     def test_get_list_of_favorites_books_books_present_returned(self):
         collector = BooksCollector()
         collector.add_new_book('favorite_book1')
